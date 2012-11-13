a143200375acd1ea28bb06e63696afb39efdbb6c
Tyler Bird <tyler.bird@gmail.com>
2012-11-12 23:25:13 -0700

    remove email from User model before to make room for it in Devise

--- db/migrate/20121106035107_create_users.rb
+++ db/migrate/20121106035107_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email
 
       t.timestamps
     end
