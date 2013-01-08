8221ad490b5607f73475969591bc24fcbf09c68c
Brandon Toone <brandon@rdlk.co>
2013-01-07 19:24:06 -0700

    Adds spec support for devise and an initial spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,4 +35,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Support for devise
+  config.include Devise::TestHelpers, :type => :controller
+  config.extend ControllerMacros, :type => :controller
 end
