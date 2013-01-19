6af501890ef7b987cc5317fb48faefb08f9327fd
Tyler Bird <tyler.bird@gmail.com>
2013-01-19 16:52:53 -0700

    removing secret token for increased security

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Campus::Application.config.secret_token = '88a9ab1a6ee696757d405446422703f602dc4489301ea121c281c96c625b8517a22f90dbcb59107022d13a3e264842752ab0e529cf59a5bb031798e2d68593d5'
