447ca6184c04b83bd37d1064cc84c3bca6fdf072
Brandon Toone <brandon@rdlk.co>
2013-01-21 16:56:40 -0700

    Re-factor

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     first_name "John"
     last_name "Doe"
-    email "john@example.com"
+    sequence(:email) {|n| "person#{n}@example.com" }
     password "password"
     password_confirmation "password"
   end
