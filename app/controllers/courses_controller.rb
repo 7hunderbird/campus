a5ccd11a8fba14eac3a4e57807adedc2bf6bc5ef
JoeReis <josephreis@gmail.com>
2013-01-27 19:24:36 -0700

    will_paginate works for 20 records per page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,7 +4,6 @@ class CoursesController < ApplicationController
   def index
     @courses = Course.paginate(page: params[:page], :per_page => 20)
 
-
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @courses }
