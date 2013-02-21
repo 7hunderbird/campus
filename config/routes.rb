ea4c210974261d5b035942d2d4f9f05f54c6bf4e
Tyler Bird <tyler.bird@gmail.com>
2013-02-20 18:24:49 -0700

    adding static pages for beta wall

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Campus::Application.routes.draw do
 
+  get "static_pages/home"
+  get "static_pages/about"
+  get "static_pages/privacy"
+  get "static_pages/contact"
+
+  match 'home',    :to => 'static_pages#home'
+  match 'about',   :to => 'static_pages#about'
+  match 'privacy', :to => 'static_pages#privacy'
+  match 'contact', :to => 'static_pages#contact' 
+
   devise_for :users
 
   resources :users
 
   resources :study_plans
-
-  
 
   resources :courses do
     resources :materials
@@ -26,7 +34,7 @@ Campus::Application.routes.draw do
     resources :study_plans
   end
 
-  root :to => 'courses#index'
+  root :to => 'static_pages#home'
 
   # web interface for testing email delivery in development
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
