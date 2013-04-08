3cd04188ca14746c9be317808b6326ad82daf33d
claront@gmail.com <claront@gmail.com>
2013-04-08 17:19:25 -0600

    Validations

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -25,4 +25,14 @@ describe Relationship do
     its(:follower) { should == follower }
     its(:followed) { should == followed }
   end
+
+  describe "when followed id is not present" do
+    before { relationship.followed_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when follower id is not present" do
+    before { relationship.follower_id = nil }
+    it { should_not be_valid }
+  end
 end
