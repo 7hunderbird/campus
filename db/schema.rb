b444ed9439d1eb06dc84befa1154707d158378b2
Tyler Bird <tyler.bird@gmail.com>
2013-06-20 10:31:11 -0600

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -142,7 +142,8 @@ ActiveRecord::Schema.define(:version => 20130530020251) do
     t.datetime "updated_at",                             :null => false
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"    t.datetime "reset_password_sent_at"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
